615446123a7e70dacd9cd2e53fe9c2b7115d3b17
litaojin <litaojin@gmail.com>
2014-11-07 11:53:07 +0800

    add signup：login name verification

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -19,13 +19,37 @@ Controllers.controller('LoginController', function($scope, $http, $location){
     });
 
 Controllers.controller('SignupController', function($scope, $http, $location, UserService){
+    $scope.signup_tip = '请设置您的用户名及密码';
         $scope.submit_signup = function(){
-            console.log('Signup');
-            UserService.save({}, {
-                login: $scope.login_name,
-                password: $scope.login_pswd
-            });
-            $location.path('/login');
+
+            if( !$scope.login_name || !$scope.login_pswd || !$scope.login_pswd_confirm){
+                $scope.signup_tip = '请填完所有信息';
+            }else if($scope.login_pswd !== $scope.login_pswd_confirm){
+                $scope.signup_tip = '密码不一致';
+            }else if($scope.login_name !== encodeURIComponent($scope.login_name)){
+                $scope.signup_tip = '用户名包含非法字符';
+            }else if($scope.login_name !== $scope.login_name.toLowerCase()){
+                $scope.signup_tip = '用户名必须是小写字母';
+            }else{
+                console.log('Signup');
+
+                UserService.get({login: $scope.login_name
+                    } , function (resp){
+                        if(resp.login){
+                            $scope.signup_tip = '用户名已经存在';
+                        }else{
+                            UserService.save({}, {
+                                login: $scope.login_name,
+                                password: $scope.login_pswd
+                            });
+                            $location.path('/login');
+                        }
+
+                    }, function(resp){
+
+                });
+
+            }
         }
     });
 
