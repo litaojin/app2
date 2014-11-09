a863747f921461ebeff0e351474af25e9784b0ac
litaojin <litaojin@gmail.com>
2014-11-09 13:39:57 +0800

    Clean up milestone

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/:id', function(req, res) {
-  res.send('Get a user list...'+ req.params.id);
-});
-
-module.exports = router;
